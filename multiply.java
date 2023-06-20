dfd83c96f090296ad282aad129a7f78734479a6d
Rahul Gupta <9178900497rs@gmail.com>
2023-06-20 15:00:16 +0530

    commited multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a= 5;
+        int b=7;
+        int mul=a*b;
+        System.out.print("the multiply is "+mul);
+     }
+}
